2d33875a23756720c7721e3ab755ba08afded161
chuong-lee <chuongldps37998@gmail.com>
2025-06-03 22:50:28 +0700

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-imports: [ConfigModule.forRoot({isGlobal: true}),ProductsModule],
+imports: [ConfigModule.forRoot({isGlobal: true}),],
 controllers: [],
 providers: [],
 })
--- backend/src/products/products.repository.ts
+++
@@ -1,5 +0,0 @@
-// import { Repository } from "typeorm";
-
-// export class ProductsRepository{
-//     constructor(private readonly productRepository: Repository);
-// }

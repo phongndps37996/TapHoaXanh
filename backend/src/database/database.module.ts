98774a9ec15390674612956668ac516bb1f3fc3e
chuong-lee <chuongldps37998@gmail.com>
2025-06-09 16:27:40 +0700

    fix: config DB auto sync

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -13,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.getOrThrow('MYSQL_USERNAME'),
         password: configService.getOrThrow('MYSQL_PASSWORD'),
         autoLoadEntities: true,
-        synchronize: configService.get('MYSQL_SYNCHRONIZE') === 'false',
+        synchronize: configService.get('MYSQL_SYNCHRONIZE') === 'true',
         migrationsRun: false,
         logging: false,
         timezone: '+07:00',

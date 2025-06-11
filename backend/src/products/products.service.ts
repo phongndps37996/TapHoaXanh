3204bde2a2a50be4e004bd5edab5dbdc22c544ad
chuong-lee <chuongldps37998@gmail.com>
2025-06-11 23:57:07 +0700

    api product
    tạo sản phẩm và get sp

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,15 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const product = this.productRepository.create({
+      name: createProductDto.name,
+      price: createProductDto.price,
+      discount: createProductDto.discount,
+      images: createProductDto.images,
+      slug: createProductDto.slug,
+      barcode: createProductDto.barcode,
+      expiry_date: createProductDto.expiry_date,
+      origin: createProductDto.origin,
+      weight_unit: createProductDto.weight_unit,
+      description: createProductDto.description,
+      quantity: createProductDto.quantity,
+    });
+
+    const saveProduct = this.productRepository.save(product);
+    return saveProduct;
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return await this.productRepository.find();
   }
 
   findOne(id: number) {

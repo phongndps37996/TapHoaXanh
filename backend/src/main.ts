080e29b494a9f5a54b59427a18c8db5ed0b920ce
phongndps37996 <phongndps37996@gmail.com>
2025-06-21 12:23:45 +0700

    doitenswagger
    doi ten swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
-  })
+    origin: '*',
+  });
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('chuongngu')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);

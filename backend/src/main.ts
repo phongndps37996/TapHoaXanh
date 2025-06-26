b1c4f6c136c772da3ba9d0930b4f4ac2da0f789d
kietej <phamtuankiet10022k5@gmail.com>
2025-06-27 02:52:39 +0700

    giao diện trang prdetail

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
+    credentials: true,
   });
   const config = new DocumentBuilder()
     .setTitle('Cats example')
